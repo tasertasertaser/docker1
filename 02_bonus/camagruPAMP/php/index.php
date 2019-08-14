eb17a8a9a2bc512b46e608b51704332f8e302f51
tasertasertaser <cschulle@student.42.us.org>
2019-08-13 22:39:01 -0700

    fixed PAMP, added three more bonuses

---
+++ 02_bonus/camagruPAMP/php/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Camagru</title>
+    </head>
+
+    <body>
+        <h1>Camagru environment setup</h1>
+        <ul>
+        </ul>
+        <i> site under construction </i>
+    </body>
+</html>
